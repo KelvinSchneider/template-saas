0242cf4f757b75b43c7556c1eac55e2c5bbc323a
Kelvin Schneider <kelvin.schneider@hypeone.com>
2025-04-24 00:23:30 -0300

    fix build

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -4,5 +4,5 @@ export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
     const metadata = paymentData.metadata;
     const userEmail = metadata?.userEmail;
     const testeId = metadata?.testeId;
-    console.log("Pagamento aprovado", paymentData);
+    console.log("Pagamento aprovado", { paymentData, userEmail, testeId });
 }
